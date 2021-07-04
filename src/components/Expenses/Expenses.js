32c9a6a20545b43760334975ff945d72e763e3cd
Aditya Galande <adityagalande14@gmail.com>
2021-07-04 22:49:19 +0530

    new component added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,34 @@
 import React, { useState } from "react";
 import "./Expenses.css";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import ExpenseFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList";
 
 function Expenses(props) {
   const [filteredYar, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
-    console.log("Expenses.js");
+    console.log("Expenses.js" + selectedYear);
     setFilteredYear(selectedYear);
   };
+
+  const filterExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYar;
+  });
+
+  // Here we check condition if data is present or not
+  // let expensesContent = <p>No Expenses Found.</p>;
+
+  // if (filterExpenses.length > 0) {
+  //   expensesContent = filterExpenses.map((expense) => (
+  //     <ExpenseItem
+  //       key={expense.id}
+  //       title={expense.title}
+  //       amount={expense.amount}
+  //       date={expense.date}
+  //     />
+  //   ));
+  // }
 
   return (
     <div>
@@ -19,26 +37,10 @@ function Expenses(props) {
           selected={filteredYar}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+
+        {/* here we use abouv condition */}
+        {/* {expensesContent} */}
+        <ExpensesList item={filterExpenses} />
       </Card>
     </div>
   );
