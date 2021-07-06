02cc769c689299a3760ce6a3b8dd0b179eed8e9d
Aditya Galande <adityagalande14@gmail.com>
2021-07-06 15:09:58 +0530

    Pie Chart

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,13 +2,16 @@ import "./Chart.css";
 import ChartBar from "./ChartBar";
 
 function Chart(props) {
+  //This below fun for converting string value to numeric value, That is used in next fun to get max of that array...
+  const dataPointsValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointsValues);
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
-          maxValue={null}
+          maxValue={totalMaximum}
           label={dataPoint.label}
         />
       ))}
